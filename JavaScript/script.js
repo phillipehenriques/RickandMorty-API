155878192e1b224558a2d34dadd751f89c286857
phillipe henriques <phillipe_ch@hotmail.com>
2022-09-18 01:08:45 -0300

    alteração

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,3 @@
-
-
 function getAllCaracters() {
     return axios.get('https://rickandmortyapi.com/api/character')
         .then(response => response)
@@ -9,11 +7,13 @@ function getAllCaracters() {
 // const personagens = getAllCaracters();
 
 // personagens.forEach((element) => {
-//     console.log(element)
+//     element.result.image
 // })
 
+const photoCharacter = document.getElementById('photo').innerHTML = personagens.results.image
 const nameCharacter = document.getElementById('name')
 const classCharacter = document.getElementById('class')
 const colorBox = document.getElementById('color')
 const statusCharacter = document.getElementById('status')
 const localCharacter = document.getElementById('local')
+
