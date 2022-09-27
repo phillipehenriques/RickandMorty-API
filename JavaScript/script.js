eadced6d68b4f487c9854b6f1034af7403714865
phillipe henriques <phillipe_ch@hotmail.com>
2022-09-26 22:01:07 -0300

    variaveis tratadas, função completa

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,43 @@
-function getAllCaracters() {
-    return axios.get('https://rickandmortyapi.com/api/character')
-        .then(response => response)
-        .catch(error => error);
+var getAllCharacters = async function () {
+    var allCharacters = [];
+
+    for(var index = 1; index <= 42; index++){
+        var newCharacters = await fetch(`https://rickandmortyapi.com/api/character?page=${index}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                return response['results']
+            })
+            .catch((ex) => {
+                console.log(ex);
+            });
+        
+        newCharacters.forEach(element => {
+            allCharacters.push(element);
+        });
+    }
+    console.log(allCharacters);
 }
 
-const personagens = getAllCaracters();
+getAllCharacters();
 
-personagens.forEach((element) => {
-    element.result.image
-})
+// function getAllCaracters() {
+//     return axios.get('https://rickandmortyapi.com/api/character')
+//         .then(response => response)
+//         .catch(error => error);
+// }
 
-document.getElementById('photo').innerHTML = personagens['results']['image']
-document.getElementById('name').innerHTML = personagens['results']['name']
-document.getElementById('class').innerHTML = personagens['results']['']
-document.getElementById('color').innerHTML = personagens['results']['']
-document.getElementById('status').innerHTML = personagens['results']['']
-document.getElementById('local').innerHTML = personagens['results']['']
+// const personagens = getAllCaracters();
+
+// personagens.forEach((element) => {
+//     element.result.image
+// })
+
+// document.getElementById('photo').innerHTML = personagens['results']['image']
+// document.getElementById('name').innerHTML = personagens['results']['name']
+// document.getElementById('class').innerHTML = personagens['results']['']
+// document.getElementById('color').innerHTML = personagens['results']['']
+// document.getElementById('status').innerHTML = personagens['results']['']
+// document.getElementById('local').innerHTML = personagens['results']['']
 
